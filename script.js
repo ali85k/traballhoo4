49e1faaf7fcee1ed619ad8c505dc94ef01a256cb
ali85k <aliceanndraad56@gmail.com>
2022-06-30 23:03:04 -0300

    first-commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function oie(){
+    console.log("Turo bom,Alice?")
+  }
+ 
+ ola()
+ 
+ 
+  function nome(Alice){
+    console.log(`Bem-vinda, ${Alice}!`)
+  }
+ 
+ 
+ function about(name, age, music){
+      console.log(`Meu nome é ${Alice}, tenho ${19} anos e gosto de ouvir ${Pop}!`)
+  }
+ 
+ 
+  function popCulture(movie, music){
+    console.log(`Meu filme favorito é ${Hereditário} e minha música favorita é ${Rude boy}`)
+ }
+ 
+ 
+ // function conta(x){
+ //     return x *3
+ // }
+ 
+ // console.log(conta(3
